2a31a4e7749ff024294ac526a33ca11803f63101
peppersmoke42 <peterchr102@gmail.com>
2024-05-08 04:05:14 -0600

    Test cases

---
+++ code.test.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const jsc = require('jsverify');
+const assert = require('assert');
+eval(fs.readFileSync('code.js')+'');
+
+const graph1 = [
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 1, 0]
+];
+
+const graph2 = [
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 1, 0]
+];
+
+const graph3 = [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+];
+
+const graph4 = [
+    [0, 1, 1, 1],
+    [1, 0, 1, 1],
+    [1, 1, 0, 1],
+    [1, 1, 1, 0]
+];
+
+const graph5 = [
+    [0, 1, 1, 1],
+    [1, 0, 1, 0],
+    [1, 1, 0, 1],
+    [1, 0, 1, 0]
+];
+
+const graph6 = [
+    [0, 1, 0, 1],
+    [1, 0, 1, 0],
+    [0, 1, 0, 1],
+    [1, 0, 1, 0]
+];
+
+// Test the function
+assert.strictEqual(are_isomorphic(graph1, graph2), true);
+assert.strictEqual(are_isomorphic(graph1, graph3), false);
+assert.strictEqual(are_isomorphic(graph1, graph4), false);
+assert.strictEqual(are_isomorphic(graph4, graph5), false);
+assert.strictEqual(are_isomorphic(graph5, graph6), false);
+
+
